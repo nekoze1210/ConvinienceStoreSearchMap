116519ee9581c415c3320ffbf1d8ad463264b674
Daiki Nagaoka <neko.z.n1210@gmail.com>
2020-02-28 01:23:33 +0900

    add comment

--- plugins/loadMapsJavaScriptApi.js
+++ plugins/loadMapsJavaScriptApi.js
@@ -1,10 +1,13 @@
 import { Loader } from 'google-maps'
 
+// .env に Maps JavaScript API が有効になった API KEY を定義すること
+const API_KEY = process.env.API_KEY
+
 export default async (_context, inject) => {
   const options = {
     libraries: ['geometry', 'places', 'visualization']
   }
-  const loader = new Loader(process.env.API_KEY, options)
+  const loader = new Loader(API_KEY, options)
   const google = await loader.load()
 
   inject('google', google)
